ac0d59bcc3d8b69ceff07bfc09f5b65b4d900c22
dbco0409 <75134167+dbco0409@users.noreply.github.com>
2025-10-14 15:04:24 +0900

    1013 다시 업로드

---
+++ ex1013/src/main/java/com/example/demo/controller/SimpleRestController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleRestController {
+    // 5-2-1 첫번째 컨트롤러
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello <strong>Backend</strong>";
+    }
+
+    // 5-2-2 첫번째 컨트롤러
+    @RequestMapping("/bye")
+    public String bye() {
+        return "Bye";
+    }
+
+    @RequestMapping("/article")
+    public String createAritcle(@RequestParam("title") String title, @RequestParam("content") String content) {
+        return String.format("title = %s / content = %s", title, content);
+    }
+}
